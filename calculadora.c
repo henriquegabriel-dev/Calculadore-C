83f524517f5f05866090e956c95c1869f435d167
henriquegabriel-dev <henrique.gabriel.silva.25122006@gmail.com>
2026-03-01 10:05:06 -0400

    Preparar_Operadores

--- calculadora.c
+++ calculadora.c
@@ -11,7 +11,10 @@ int main (){
     fgets(input, 100, stdin);
 
     int numero_atual = 0;
-    
+    char operador[1];
+    int primeiro_numero;
+
+    int primeiro_bloco;
 
     int i;
     for(i = 0; i < strlen(input); i++){ //junta os numeros e os converte de caracteres para numeros.
@@ -20,11 +23,18 @@ int main (){
 
            numero_atual = numero_atual * 10 + (input[i] - '0');
 
+        }else if(input[i] == '+'){
+            
+        }else if(input[i] == '-'){
+
+        }else if(input[i] == '/'){
+
+        }else if(input[i] == '*'){
+
         }
         
     
     }
-    printf("%d", numero_atual);
-
+    
     return 0;
 }
