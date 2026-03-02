2c8321b619770c31071ce78d06b017708521b24b
henriquegabriel-dev <henrique.gabriel.silva.25122006@gmail.com>
2026-03-02 18:41:28 -0400

    problema_de_bloco_de_conta_problema(for)

--- calculadora.c
+++ calculadora.c
@@ -6,13 +6,13 @@ int main (){
     setlocale(LC_ALL, "");
 
     char input [100];
-   
+
 
     while (1)
     {
         printf("\n\nDigite seus valores e digite suas operações (digite sair, para fechar o programa):\n");
         fgets(input, 100, stdin);
-    
+
         if(strcmp(input, "sair\n") == 0){
             exit(0);
         }
@@ -21,12 +21,11 @@ int main (){
         char operador;
         int primeiro_numero = 0;
 
-        int primeiro_bloco;
-        int conta;
-
+        int primeiro_bloco = 0;
+        int conta = 0;
 
         int i;
-        for(i = 0; i < strlen(input); i++){ 
+        for(i = 0; i < strlen(input); i++){ //*FOR ESTA ESTA PEDENDO A INFORMAÇÃO DO PRIMEIRO NUMERO, SÓ ACEITA UM BLOCO*//
 
             if(input[i] >= '0' && input[i] <= '9'){     //junta os numeros e os converte de caracteres para numeros.
 
@@ -39,26 +38,35 @@ int main (){
                 operador = input[i];
                 numero_atual = 0;
             }
-            
-        }
 
+        }
             if(operador == '+'){
                 conta = primeiro_numero + numero_atual;
+                primeiro_bloco = conta + primeiro_bloco;
             }
 
-            else if(operador == '-'){
+            /*else if(operador == '-'){
                 conta = primeiro_numero - numero_atual;
+                primeiro_bloco + conta;
             }
-            
+
             else if(operador == '/'){
                 conta = primeiro_numero / numero_atual;
+                primeiro_bloco + conta;
             }
 
             else if(operador == '*'){
                 conta = primeiro_numero * numero_atual;
+                primeiro_bloco + conta;
+            }*/
+
+            if(primeiro_bloco >! 0){
+                printf("%d", primeiro_bloco);
+            }else{
+                printf("%d", conta);
             }
-        
-            printf("%d", conta);
+
+
     }
         return 0;
 }
